bcf4ebe3b8a28d6c92719199cdc352a97cc2bc55
Lars Thorup <lars@zealake.com>
2023-06-05 16:37:38 +0200

    clean code

--- src/Count.test.jsx
+++ src/Count.test.jsx
@@ -4,9 +4,8 @@ import Count from "./Count.jsx";
 
 describe(Count.name, () => {
   it("should render", () => {
-    const testRenderer = TestRenderer.create(<Count color="black" />);
-    const html = testRenderer.toJSON();
-    expect(html).toMatchInlineSnapshot(`
+    const { toJSON, root } = TestRenderer.create(<Count color="black" />);
+    expect(toJSON()).toMatchInlineSnapshot(`
       <button
         onClick={[Function]}
       >
@@ -21,16 +20,14 @@ describe(Count.name, () => {
         </span>
       </button>
     `);
-    const { root } = testRenderer;
     expect(root.type).toBe(Count);
     expect(root.props).toEqual({ color: "black" });
-    const [button] = root.children;
-    expect(button.type).toBe("button");
+    const button = root.findByType("button");
     expect(button.props).toHaveProperty("onClick");
-    const [span] = button.children;
+    const span = button.props.children;
     expect(span.type).toBe("span");
     expect(span.props).toHaveProperty("style", { color: "black" });
-    expect(span.children).toEqual(["count is 0"]);
+    expect(span.props.children).toEqual("count is 0");
   });
 
   it("should let user click to increment count", () => {
@@ -40,12 +37,12 @@ describe(Count.name, () => {
       renderer = TestRenderer.create(<Count color="black" />);
     });
     const { root } = renderer;
-    const [button] = root.children;
+    const button = root.findByType("button");
 
     // when: click button
     button.props.onClick();
 
     // then: component re-renders with incremented count
-    expect(button.children[0].children).toEqual(["count is 1"]);
+    expect(button.props.children.props.children).toEqual("count is 1");
   });
 });
